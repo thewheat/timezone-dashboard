d391c61716bc89b75babdd3d0686f44cbcda25b0
Timothy Lim <tim.lim@intercom.io>
2018-09-24 15:22:24 +0800

    Fix offline support

--- sw.js
+++ sw.js
@@ -4,18 +4,18 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => {
       return cache.addAll([
-       '/',
-       '/index.html',
-       '/bower_components/bootstrap/dist/css/bootstrap.min.css',
-       '/bower_components/jquery/dist/jquery.min.js',
-       '/bower_components/handlebars/handlebars.js',
-       '/bower_components/bootstrap/dist/js/bootstrap.min.js',
-       '/bower_components/moment/min/moment.min.js',
-       '/bower_components/moment-timezone/builds/moment-timezone-with-data.min.js',
-       '/bower_components/typeahead.js/dist/typeahead.bundle.min.js',
-       '/bower_components/ember/ember.min.js',
-       '/bower_components/ember-data/ember-data.min.js',
-       '/bower_components/ember/ember-template-compiler.js'
+       './',
+       './index.html',
+       './bower_components/bootstrap/dist/css/bootstrap.min.css',
+       './bower_components/jquery/dist/jquery.min.js',
+       './bower_components/handlebars/handlebars.js',
+       './bower_components/bootstrap/dist/js/bootstrap.min.js',
+       './bower_components/moment/min/moment.min.js',
+       './bower_components/moment-timezone/builds/moment-timezone-with-data.min.js',
+       './bower_components/typeahead.js/dist/typeahead.bundle.min.js',
+       './bower_components/ember/ember.min.js',
+       './bower_components/ember-data/ember-data.min.js',
+       './bower_components/ember/ember-template-compiler.js'
       ])
           .then(() => self.skipWaiting());
     })
